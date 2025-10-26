5fd6ac1389b2b0b0a54fa64f0bfb70fda2e5120a
laZy1806 <kylegosine@gmail.com>
2025-10-26 00:10:29 -0400

    Addition of basic React-router-dom and components for sign in page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/SignIn";
+import RootLayout from "./layouts/RootLayout";
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+const apikey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const url = import.meta.env.VITE_SUPABASE_URL;
+
+const supabase = createClient(url, apikey);
+
+console.log(url)
+
 
 function App() {
   const [instruments, setInstruments] = useState([]);
 
+  
   useEffect(() => {
     getInstruments();
   }, []);
 
   async function getInstruments() {
-    const { data } = await supabase.from("instruments").select();
+    const { data } = await supabase.from("users").select('*');
+    console.log(data)
     setInstruments(data);
   }
+  
+
+  console.log("hi?")
 
   return (
-    <ul>
-      {instruments.map((instrument) => (
-        <li key={instrument.name}>{instrument.name}</li>
-      ))}
-    </ul>
+    <BrowserRouter>
+      <Routes>
+        <Route element = {<RootLayout/>}>
+
+          <Route index path = "/" element = {<SignInPage/>}/>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,16 @@
+function SignInPage(){
+
+
+    return(
+        <div>
+            <div style={{}}>i like my dog he is really cool.</div>
+        </div>
+    )
+
+}
+
+
+
+
+
+export default SignInPage;
